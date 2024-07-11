cae62b1d8f600c5a585b670b9973e10913eddd0c
zhuyinghao <zhuyinghao@qiyi.com>
2024-07-11 02:19:24 +0000

    add read prompts from txt

---
+++ run.sh
@@ -0,0 +1,3 @@
+# python3 scripts/sample.py "一对中国的年轻的夫妻在客厅里打游戏，他们的颜值很高，希望可以展现客厅的全貌，高质量，高清，电影海报质感" /data/yh/TEXT2IMG/Kolors/result/1.jpg
+# python3 scripts/sample_with_txt.py /data/yh/TEXT2IMG/Kolors/prompts/prompt_2.txt /data/yh/TEXT2IMG/Kolors/result/prompts_2_gai
+python3 scripts/sample_with_txt.py /data/yh/TEXT2IMG/Kolors/prompts/prompt_3.txt /data/yh/TEXT2IMG/Kolors/result/prompts_3
--- scripts/sample_with_txt.py
+++ scripts/sample_with_txt.py
@@ -8,8 +8,7 @@ from diffusers import EulerDiscreteScheduler
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def infer(prompt, save_path):
-    # ckpt_dir = f'{root_dir}/weights/Kolors'
+def load_model():
     ckpt_dir = '/mnt/ec-data2/ivs/1080p/zyh/kolor/weights/Kolors'
     text_encoder = ChatGLMModel.from_pretrained(
         f'{ckpt_dir}/text_encoder',
@@ -27,18 +26,36 @@ def infer(prompt, save_path):
             force_zeros_for_empty_prompt=False)
     pipe = pipe.to("cuda")
     pipe.enable_model_cpu_offload()
+    return pipe
+
+def infer(pipe, prompt, save_path):
     image = pipe(
         prompt=prompt,
-        height=1024,
-        width=1024,
+        height=1280,
+        width=720,
         num_inference_steps=50,
         guidance_scale=5.0,
         num_images_per_prompt=1,
-        generator= torch.Generator(pipe.device).manual_seed(50)).images[0]
-    # image.save(f'{root_dir}/scripts/outputs/sample_test.jpg')
+        generator= torch.Generator(pipe.device).manual_seed(66)).images[0]
     image.save(save_path)
+    
+def process_prompts(input_file, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    pipe = load_model()
+    
+    with open(input_file, 'r', encoding='utf-8') as file:
+        prompts = file.readlines()
+    
+    for i, prompt in enumerate(prompts):
+        prompt = prompt.strip()  # 去除行尾的换行符
+        if prompt:
+            output_path = os.path.join(output_folder, f"image_{i+1}.png")
+            infer(pipe, prompt, output_path)
+            print(f"Generated image for prompt '{prompt}' at '{output_path}'")
 
 
 if __name__ == '__main__':
     import fire
-    fire.Fire(infer)
+    fire.Fire(process_prompts)
